8d68595055dcc19f08d8201f6ea6dd1ac0a2b97f
joão melo <j@melo.plus>
2022-06-25 15:28:16 -0300

    firebase config for the web app

---
+++ firebase.config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyANgZQOPppStrJ8LFFA41SH-t3S9aciw9k",
+  authDomain: "fireui-fccc0.firebaseapp.com",
+  projectId: "fireui-fccc0",
+  storageBucket: "fireui-fccc0.appspot.com",
+  messagingSenderId: "500181612215",
+  appId: "1:500181612215:web:d121e0046d6b70ae92ff32",
+};
