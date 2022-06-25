3bb657b704374c4c80c7f0003b8e877325ae9788
joão melo <j@melo.plus>
2022-06-25 20:32:53 -0300

    welcome page has profile data and sign out

--- page-welcome.svelte
+++ page-welcome.svelte
@@ -1,1 +1,11 @@
-Welcome Page
+<script>
+  import auth from './firebase.auth';
+  const handleSignOut = () => auth.signOut();
+
+  export let id, email;
+</script>
+
+<p>Welcome { email }!</p>
+<p>Your id is { id }</p>
+<button on:click={handleSignOut}>Sign Out</button>
+
