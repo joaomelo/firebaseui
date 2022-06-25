eb16ee9cbf8b1d0b988d9939a03904f98ba654fe
joão melo <j@melo.plus>
2022-06-25 16:56:25 -0300

    firebase integration

---
+++ firebase.auth.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import * as firebaseui from "firebaseui";
+import "firebaseui/dist/firebaseui.css";
+import config from "./firebase.config";
+
+const app = firebase.initializeApp(config);
+const auth = app.auth();
+const ui = new firebaseui.auth.AuthUI(auth);
+
+export default {
+  signOut: () => "TODO",
+  onChanged: () => "TODO",
+  uiStart: () => "TODO",
+};
